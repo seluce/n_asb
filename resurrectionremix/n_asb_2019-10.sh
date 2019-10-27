3c94578dd1d75f5f15fa4593401ec2ee360fe0ef
seluce <36071735+seluce@users.noreply.github.com>
2019-10-27 08:08:52 +0100

    typo fix for 2019-10-05

--- resurrectionremix/n_asb_2019-10.sh
+++ resurrectionremix/n_asb_2019-10.sh
@@ -4,7 +4,7 @@ cd build
 git fetch "https://github.com/LineageOS/android_build" refs/changes/49/260549/1 && git cherry-pick FETCH_HEAD
 cd ..
 cd frameworks/base
-git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/50/260550/1 && git format-patch -1 --stdout FETCH_HEAD
+git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/50/260550/1 && git cherry-pick FETCH_HEAD
 cd ../..
 cd frameworks/av
 git fetch "https://github.com/LineageOS/android_frameworks_av" refs/changes/52/260552/1 && git cherry-pick FETCH_HEAD
