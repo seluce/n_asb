a29630b63b8b792ac43b91b94977c0b7937643c1
seluce <36071735+seluce@users.noreply.github.com>
2019-11-17 13:38:41 +0100

    add cd to default in script

--- crdroid/apply-patches.sh
+++ crdroid/apply-patches.sh
@@ -6,6 +6,8 @@ sleep 1
 echo -e "\n start patching. Wait some minutes.. (depends on your internet connection)"
 sleep 2
 
+# merge security patches
+
 cd $CURRENT_DIR
 bash n_asb_2018-08.sh
 
@@ -54,8 +56,13 @@ bash n_asb_2019-10.sh
 cd $CURRENT_DIR
 bash n_asb_2019-11.sh
 
+# security patches are merged
+
 sleep 1
 echo -e "\n all patches are included now"
 sleep 2
 echo -e "\n bump security patch level to 2019-11-05"
 echo -e "\n script by seluce"
+
+# return to default directory 
+cd ../../../../../..
--- resurrectionremix/apply-patches.sh
+++ resurrectionremix/apply-patches.sh
@@ -6,6 +6,8 @@ sleep 1
 echo -e "\n start patching. Wait some minutes.. (depends on your internet connection)"
 sleep 2
 
+# merge security patches
+
 cd $CURRENT_DIR
 bash n_asb_2018-01.sh
 
@@ -75,8 +77,13 @@ bash n_asb_2019-10.sh
 cd $CURRENT_DIR
 bash n_asb_2019-11.sh
 
+# security patches are merged
+
 sleep 1
 echo -e "\n all patches are included now"
 sleep 2
 echo -e "\n bump security patch level to 2019-11-05"
 echo -e "\n script by seluce"
+
+# return to default directory 
+cd ../../../../../..
